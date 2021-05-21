630c18589e517f19709815e469154d80e99611d5
Jeffry Raymond <jeffryraymond96@gmail.com>
2021-05-20 22:47:56 -0700

    Removed example route

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,5 @@ const controllers = require('./../controllers/controllers');
 
 // Defining routes - localhost:5000/api/v1..
 router.get('/analytics', controllers.getAnalytics);
-router.get('/hello', controllers.hello);
 
 module.exports = router;
